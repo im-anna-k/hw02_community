5bf3f8fdbe1ab1368502dd8c89c5eded1b7a9223
im-anna-k <im.anna.k.1999@gmail.com>
2022-02-22 21:38:01 +0300

    check

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('group_list/<slug:slug>/', views.group_posts, name='group_posts'),
     path('group_list.html/<slug:slug>/', views.group_posts,
          name='group_posts'),
+    path('group/<slug>/', views.group_posts, name='group_posts'),
 ]
