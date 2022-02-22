e4e80673d74023018a52678eb97ce2ba33d3e0ea
im-anna-k <im.anna.k.1999@gmail.com>
2022-02-22 21:26:13 +0300

    check

---
+++ yatube/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'Posts'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('group_list/<slug:slug>/', views.group_posts, name='group_posts'),
+    path('group_list.html/<slug:slug>/', views.group_posts,
+         name='group_posts'),
+]
