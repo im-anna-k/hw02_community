7fecdbc9c03755befa007b471187be0a5cb21317
im-anna-k <im.anna.k.1999@gmail.com>
2022-05-07 01:16:26 +0300

    email

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -88,3 +88,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.AppDirectoriesFinder',
                        'django.contrib.staticfiles.finders.FileSystemFinder',)
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+LOGOUT_REDIRECT_URL = 'posts:index'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
