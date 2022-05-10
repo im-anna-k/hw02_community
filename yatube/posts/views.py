0665c1ad770de32595a3de9e8909e5683a8322c0
im-anna-k <im.anna.k.1999@gmail.com>
2022-05-10 23:47:44 +0300

    paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
@@ -6,9 +7,12 @@ POSTS_PER_PAGE = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:POSTS_PER_PAGE]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
