b2be5c316351c2fb6a6b4630937dea9d58bfe816
im-anna-k <im.anna.k.1999@gmail.com>
2022-05-12 21:38:37 +0300

    template_view

--- yatube/about/urls.py
+++ yatube/about/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'about'
 
 urlpatterns = [
-    path('author/', views.AboutAuthor.as_view()),
-    path('tech/', views.AboutTech.as_view())
+    path('author/', views.AboutAuthorView.as_view(), name='author'),
+    path('tech/', views.AboutTechView.as_view(), name='tech'),
 ]
